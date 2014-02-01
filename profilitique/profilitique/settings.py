a823efd606726c67bb51236cf1c93d5a34f3d5fd
Pablo SEMINARIO <pablo@seminar.io>
2014-02-01 12:57:24 +0100

    Changement de langue

--- profilitique/profilitique/settings.py
+++ profilitique/profilitique/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
--- profilitique/programmes/models.py
+++ profilitique/programmes/models.py
@@ -5,11 +5,22 @@ class Candidat(models.Model):
     nom = models.CharField(max_length=200)
     parti = models.CharField(max_length=200)
 
+    def __unicode__(self):
+        return self.nom
+
+
 class Thematique(models.Model):
     nom = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.nom
+
 
 class Proposition(models.Model):
     resume = models.TextField()
     description = models.TextField()
     thematique = models.ForeignKey(Thematique)
     candidat = models.ForeignKey(Candidat)
+
+    def __unicode__(self):
+        return self.nom
