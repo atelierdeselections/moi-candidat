397322cf25333db5ef91955e1dde6ed3cd1ed705
Pablo SEMINARIO <pablo@seminar.io>
2014-02-01 16:17:51 +0100

    Model Parti

--- profilitique/programmes/admin.py
+++ profilitique/programmes/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from programmes.models import Candidat, Thematique, Proposition
+from programmes.models import Parti, Candidat, Thematique, Proposition
+
 
 class CandidatAdmin(admin.ModelAdmin):
-	list_display = ('nom', 'prenom','parti')
+    list_display = ('nom', 'prenom', 'parti_list')
+    fields = (('nom', 'prenom'), 'partis')
+
+
+admin.site.register(Parti)
 admin.site.register(Candidat,CandidatAdmin)
 admin.site.register(Thematique)
 admin.site.register(Proposition)
