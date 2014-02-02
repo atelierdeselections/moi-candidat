dddaca2e4da5e739696a507694d9ef0e6eaaf1e4
DILL44 <npatard@gmail.com>
2014-02-02 17:29:46 +0100

    fin page programme

--- profilitique/programmes/static/js/anim.js
+++ profilitique/programmes/static/js/anim.js
@@ -14,3 +14,48 @@ jQuery(document).ready(function() {
 		});
 	});
 });
+
+jQuery(document).ready(function() {
+ 	jQuery('.candidat').each(function() {
+		var id_balise=this.id;
+		var id_candidat=id_balise.substring(id_balise.lastIndexOf("_")+1,id_balise.length);
+		jQuery('#candidat_'+id_candidat).click(function() {
+			jQuery('.propositions_candidat').each(function() {
+				jQuery(this).css('display','none');
+			});	
+			jQuery('#proposition_candidat_'+id_candidat).css('display','block');
+		});
+	});
+});
+
+jQuery(document).ready(function() {
+ 	jQuery('.thematique').each(function() {
+		var id_balise=this.id;
+		var id_candidat=id_balise.substring(id_balise.lastIndexOf("_")+1,id_balise.length);
+		jQuery('#thematique_'+id_candidat).click(function() {
+			jQuery('.box_proposition').each(function() {
+				jQuery(this).css('display','none');
+			});	
+			jQuery('.proposition_thematique_'+id_candidat).each(function() {
+				jQuery(this).css('display','block');
+			});
+		});
+	});
+});
+
+
+jQuery(document).ready(function() {
+		jQuery('#touts_les_candidats').click(function() {
+			jQuery('.propositions_candidat').each(function() {
+				jQuery(this).css('display','block');
+			});
+		});
+});
+
+jQuery(document).ready(function() {
+		jQuery('#toutes_les_thematique').click(function() {
+			jQuery('.box_proposition').each(function() {
+				jQuery(this).css('display','block');
+			});
+		});
+});
