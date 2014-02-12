4e17983ff05b4406ca84d8234535c2f8585388d9
Pablo SEMINARIO <pablo@seminar.io>
2014-02-12 22:21:00 +0100

    Serve media files with DEBUG=True

--- moi_candidat/moi_candidat/urls.py
+++ moi_candidat/moi_candidat/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from programmes.views import ChoisirWizard
 
 from django.contrib import admin
@@ -17,5 +19,4 @@ urlpatterns = patterns('',
     (r'^choisir/$', ChoisirWizard.as_view()),
 	url(r'^resultat/$', 'programmes.views.resultat'),
 
-
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
