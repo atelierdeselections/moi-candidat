711cb3ad3fc4bba6c246ebb5e27bec06c72b2cb2
DILL44 <npatard@gmail.com>
2014-02-22 13:44:27 +0100

    fixe anim programme page

--- moi_candidat/programmes/static/js/anim.js
+++ moi_candidat/programmes/static/js/anim.js
@@ -20,9 +20,11 @@ jQuery(document).ready(function() {
 		var id_balise=this.id;
 		var id_candidat=id_balise.substring(id_balise.lastIndexOf("_")+1,id_balise.length);
 		jQuery('#candidat_'+id_candidat).click(function() {
+			jQuery('.candidat').css('background','#EEEEEE')
 			jQuery('.propositions_candidat').each(function() {
 				jQuery(this).css('display','none');
 			});	
+			jQuery('#'+id_balise).css('background','#CCCCCC');
 			jQuery('#proposition_candidat_'+id_candidat).css('display','block');
 		});
 	});
@@ -33,10 +35,13 @@ jQuery(document).ready(function() {
 		var id_balise=this.id;
 		var id_candidat=id_balise.substring(id_balise.lastIndexOf("_")+1,id_balise.length);
 		jQuery('#thematique_'+id_candidat).click(function() {
+			jQuery('.thematique').css('background','#252A3A');
+			jQuery('#toutes_les_thematique').css('background','#252A3A');
 			jQuery('.box_proposition').each(function() {
-				jQuery(this).css('display','none');
+				jQuery(this).css('display','none');	
 			});	
 			jQuery('.proposition_thematique_'+id_candidat).each(function() {
+				jQuery('#'+id_balise).css('background','#353A4A');
 				jQuery(this).css('display','block');
 			});
 		});
@@ -46,6 +51,8 @@ jQuery(document).ready(function() {
 
 jQuery(document).ready(function() {
 		jQuery('#touts_les_candidats').click(function() {
+			jQuery('.candidat').css('background','#EEEEEE');
+			jQuery('#touts_les_candidats').css('background','#CCCCCC');
 			jQuery('.propositions_candidat').each(function() {
 				jQuery(this).css('display','block');
 			});
@@ -54,6 +61,8 @@ jQuery(document).ready(function() {
 
 jQuery(document).ready(function() {
 		jQuery('#toutes_les_thematique').click(function() {
+			jQuery('.thematique').css('background','#252A3A');
+			jQuery('#toutes_les_thematique').css('background','#353A4A');
 			jQuery('.box_proposition').each(function() {
 				jQuery(this).css('display','block');
 			});
