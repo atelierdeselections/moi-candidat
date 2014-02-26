d49f43c30c765286be630cf2e6cfd032f8b994f2
Pablo SEMINARIO <pablo@seminar.io>
2014-02-26 16:34:26 +0100

    Timestamp for resultats

--- moi_candidat/programmes/models.py
+++ moi_candidat/programmes/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Resultat(models.Model):
     hashcode = models.CharField(u'Hashcode dans les URLs de partage', max_length=200)
     propositions_csv = models.CharField(u'Une liste de Propositions ID separés par des virgules sans espaces', max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return self.propositions_csv
